5d6e2584ef93922e4f9759e19a890baeef86f366
GermanBread <nd01@online.de>
2021-05-08 14:43:59 +0200

    👉👈 update (I have no idea what I changed and for some reason i get a lot of merge conflicts)

--- Program.cs
+++ Program.cs
@@ -2,79 +2,96 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 // Discord
 using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
 
 // Mate
 using Mate.Utility;
 using Mate.Variables;
 
-// Start the stopwatch
-GlobalVariables.Bootwatch.Start();
-
-string[] bootParameters = Environment.GetCommandLineArgs();
-
-// Splash
-"Discord bot-init by ".Write(ConsoleColor.DarkCyan);
-"GermanBread#9077".WriteLine(ConsoleColor.Cyan);
-
-if (bootParameters.Contains("m")) "<<< Booting maintenance mode >>>".WriteLine(ConsoleColor.Yellow);
-else "<<< Starting bot >>>".WriteLine(ConsoleColor.Green);
-
-if (bootParameters.Contains("m")) {
-    // Pause the stopwatch
-    GlobalVariables.Bootwatch.Stop();
-
-    "Please enter a valid guild ID".WriteLine();
-    ulong value;
-    while (!ulong.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.Number, null, out value)) {
-        GlobalVariables.MaintenanceModeGuild = value;
-    }
-    GlobalVariables.MaintenanceModeGuild = value;
-    $"Booting with guild ID \"{GlobalVariables.MaintenanceModeGuild}\"".WriteLine();
-
-    // Resume the stopwatch
+// Begin scope
+{
+    // Start the stopwatch
     GlobalVariables.Bootwatch.Start();
-}
 
-InitDataDirectory();
+    string[] bootParameters = Environment.GetCommandLineArgs();
 
-try {
-    // Start the bot
-    await GlobalVariables.DiscordBot.Start();
-}
-catch (Exception ex) {
-    await Logger.Log(new LogMessage(LogSeverity.Critical, "Crash", "The bot crashed!", ex));
-}
+    // Splash
+    "Discord bot-init by ".Write(ConsoleColor.DarkCyan);
+    "GermanBread#9077".WriteLine(ConsoleColor.Cyan);
 
-CreateLog();
-
-// Methods
-void InitDataDirectory() {
-    Directory.CreateDirectory(GlobalVariables.GuildProfilesPath);
-    Directory.CreateDirectory(GlobalVariables.BaseLogPath);
-    if (!File.Exists(GlobalVariables.TokenPath)) File.WriteAllText(GlobalVariables.TokenPath,"Token here");
-}
-
-void CreateLog() {
-    string logFileDate = DateTime.UtcNow.ToShortDateString().Replace('/', '-') /* had to add this because else the log saving process crashes */;
-    StreamWriter _logwriter = new StreamWriter(GlobalVariables.LogFilePath.Replace("%D", logFileDate), true);
-    _logwriter.WriteLine($"Logfile created on {DateTime.UtcNow}");
-    foreach (var element in GlobalVariables.Logs)
-    {
-        var _message = element.Message;
-        _logwriter.WriteLine($"{DateTime.Now,-19} [{_message.Severity,8}] {_message.Source}: {_message.Message} {_message.Exception}");
+    string bitVersionString = Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit";
+    string osVersionString = "unknown version";
+    string osNameString = "unknown name";
+    if (OperatingSystem.IsLinux()) {
+        if (File.Exists("/proc/version")) {
+            osVersionString = File.ReadAllText("/proc/version");
+            osVersionString = osVersionString[14..];
+            osVersionString = osVersionString[..osVersionString.IndexOf(' ')];
+        }
+        if (File.Exists("/etc/os-release")) {
+            osNameString = File.ReadAllText("/etc/os-release");
+            osNameString = Regex.Match(osNameString, "(PRETTY_NAME=\").*").Captures.First().Value[13..].TrimEnd('\"');
+        }
+        "Running on Linux ".Write(ConsoleColor.White);
+        $"{osNameString} ".Write(ConsoleColor.Red);
+        $"{bitVersionString} ".Write(ConsoleColor.White);
+        $"{osVersionString}".WriteLine(ConsoleColor.Green);
+    } else {
+        Environment.OSVersion.WriteLine(ConsoleColor.White);
     }
-    _logwriter.Close(); // Write buffer to disk
-    _logwriter.Dispose(); // Free up memory
 
-    Logger.Log(new LogMessage(LogSeverity.Info, "Shutdown", $"Log saved"));
+    if (bootParameters.Contains("m")) "<<< Booting maintenance mode >>>".WriteLine(ConsoleColor.Yellow);
+    else "<<< Starting bot >>>".WriteLine(ConsoleColor.Green);
+
+    if (bootParameters.Contains("m")) {
+        // Pause the stopwatch
+        GlobalVariables.Bootwatch.Stop();
+
+        "Please enter a valid guild ID".WriteLine();
+        ulong value;
+        while (!ulong.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.Number, null, out value)) {
+            GlobalVariables.MaintenanceModeGuild = value;
+        }
+        GlobalVariables.MaintenanceModeGuild = value;
+        $"Booting with guild ID \"{GlobalVariables.MaintenanceModeGuild}\"".WriteLine();
+
+        // Resume the stopwatch
+        GlobalVariables.Bootwatch.Start();
+    }
+
+    initDataDirectory();
+
+    try {
+        // Start the bot
+        await GlobalVariables.DiscordBot.Start();
+    }
+    catch (Exception ex) {
+        await Logger.Log(new LogMessage(LogSeverity.Critical, "Crash", "The bot crashed!", ex));
+    }
+
+    createLog();
+
+    // Methods
+    void initDataDirectory() {
+        Directory.CreateDirectory(GlobalVariables.GuildProfilesPath);
+        Directory.CreateDirectory(GlobalVariables.BaseLogPath);
+        if (!File.Exists(GlobalVariables.TokenPath)) File.WriteAllText(GlobalVariables.TokenPath, "Token here");
+    }
+    void createLog() {
+        string logFileDate = DateTime.UtcNow.ToShortDateString().Replace('/', '-') /* had to add this because else the log saving process crashes */;
+        StreamWriter _logwriter = new StreamWriter(GlobalVariables.LogFilePath.Replace("%D", logFileDate), true);
+        _logwriter.WriteLine($"Logfile created on {DateTime.UtcNow}");
+        foreach (var element in GlobalVariables.Logs)
+        {
+            var _message = element.Message;
+            _logwriter.WriteLine($"{DateTime.Now,-19} [{_message.Severity,8}] {_message.Source}: {_message.Message} {_message.Exception}");
+        }
+        _logwriter.Close(); // Write buffer to disk
+        _logwriter.Dispose(); // Free up memory
+
+        Logger.Log(new LogMessage(LogSeverity.Info, "Shutdown", $"Log saved"));
+    }
 }
