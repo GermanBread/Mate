f3c6d3559390d89ff9ad7e2290ff2ec0b201792a
GermanBread <nd01@online.de>
2021-10-04 15:05:26 +0200

    updating

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,10 @@ echo Shell scipt: Locating binary
 _basedir=$(dirname $0)
 [ ! -e $_basedir/Mate ] && echo Binary called \"Mate\" not found in $_basedir && exit 1
 while true; do
+    if [ Mate_new ]; then
+        echo Shell scipt: Updating binary
+        mv Mate_new Mate
+    fi
     echo Shell scipt: Starting bot
     ./Mate
     [ $? -eq 0 ] && exit 0
